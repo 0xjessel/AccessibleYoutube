6b6d6fdbf11b1c330aec50c802fa0f53e2d9364c
Jesse Chen <Jesse Chen@10.10.83.110>
2011-04-12 15:56:07 -0700

    Added GPL License.

--- src/net/jessechen/accessibleyoutube/Result.java
+++ src/net/jessechen/accessibleyoutube/Result.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Authors:
+ *     Jesse Chen
+ * 
+ * Copyright (c) 2011 Jesse Chen.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package net.jessechen.accessibleyoutube;
 
 import java.io.InputStream;
